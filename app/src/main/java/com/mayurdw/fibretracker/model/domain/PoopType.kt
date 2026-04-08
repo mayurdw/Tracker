26560154853adc6bcd4f9cd1322aae82947d54e4
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-04-09 07:02:10 +1200

    Description Mismatch fixed

--- app/src/main/java/com/mayurdw/fibretracker/model/domain/PoopType.kt
+++ app/src/main/java/com/mayurdw/fibretracker/model/domain/PoopType.kt
@@ -4,24 +4,24 @@ import com.mayurdw.fibretracker.R
 
 enum class PoopType(val image: Int, val title: String, val desc: String) {
     TYPE_7(
-        R.drawable.type_7, "Type 7", "Separate Hard Lumps, like little pebbles (Hard to pass)"
+        R.drawable.type_7, "Type 7", "Watery with no solid pieces"
     ),
     TYPE_6(
-        R.drawable.type_6, "Type 6", "Hard and lumpy and starting to resemble a sausage"
+        R.drawable.type_6, "Type 6", "Fluffy, mushy pieces with ragged edges"
     ),
     TYPE_5(
-        R.drawable.type_5, "Type 5", "Sausage-shaped with cracks on the surface"
+        R.drawable.type_5, "Type 5", "Soft blobs with clear cut edges\n"
     ),
     TYPE_4(
         R.drawable.type_4, "Type 4", "Thinner and more snakelike, plus smooth and soft"
     ),
     TYPE_3(
-        R.drawable.type_3, "Type 3", "Soft blobs with clear cut edges\n"
+        R.drawable.type_3, "Type 3", "Sausage-shaped with cracks on the surface\n"
     ),
     TYPE_2(
-        R.drawable.type_2, "Type 2", "Fluffy, mushy pieces with ragged edges"
+        R.drawable.type_2, "Type 2", "Hard and lumpy and starting to resemble a sausage"
     ),
     TYPE_1(
-        R.drawable.type_1, "Type 1", "Watery with no solid pieces\n"
+        R.drawable.type_1, "Type 1", "Separate Hard Lumps, like little pebbles (Hard to pass)"
     )
 }
